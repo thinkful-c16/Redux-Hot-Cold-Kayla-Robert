48336a7003d36d93fa68165e0597ce0570222ae2
Robert Frazier <robert.v.frazier@gmail.com>
2017-12-21 17:03:50 -0500

    All tests passing, finally.

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import {makeGuess} from '../actions';
 
 import {GuessForm} from './guess-form';
 
@@ -8,20 +9,35 @@ describe('<GuessForm />', () => {
     shallow(<GuessForm />);
   });
 
-  it('Should fire the onMakeGuess callback when the form is submitted', () => {
-    const callback = jest.fn();
-    const wrapper = mount(<GuessForm onMakeGuess={callback} />);
+  // it('Should fire the onMakeGuess callback when the form is submitted', () => {
+  //   const callback = jest.fn();
+  //   const wrapper = mount(<GuessForm onMakeGuess={callback} />);
+  //   const value = 10;
+  //   wrapper.find('input[type="number"]').instance().value = value;
+  //   wrapper.simulate('submit');
+  //   expect(callback).toHaveBeenCalledWith(value.toString());
+  // });
+
+  it('Should dispatch onMakeGuess callback when the form is submitted', () => {
+    const dispatch = jest.fn();
+    const wrapper = mount(<GuessForm dispatch={dispatch} />);
     const value = 10;
     wrapper.find('input[type="number"]').instance().value = value;
     wrapper.simulate('submit');
-    expect(callback).toHaveBeenCalledWith(value.toString());
+    expect(dispatch).toHaveBeenCalled();
+    const action=dispatch.mock.calls[0][0];
+    expect(dispatch).toHaveBeenCalledWith(makeGuess(value.toString()));
   });
 
+
   it('Should reset the input when the form is submitted', () => {
-    const wrapper = mount(<GuessForm />);
+    const dispatch = jest.fn();
+    const wrapper = mount(<GuessForm dispatch={dispatch} />);
     const input = wrapper.find('input[type="number"]');
     input.instance().value = 10;
     wrapper.simulate('submit');
+    expect(dispatch).toHaveBeenCalled();
+    const action=dispatch.mock.calls[0][0];
     expect(input.instance().value).toEqual('');
   });
 });
